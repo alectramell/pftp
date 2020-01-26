77eb68fc2090f9cf0ec43245bccd2d46187e9b66
alectramell <alectramell@gmail.com>
2020-01-26 11:29:34 -0800

    pftp

--- server.py
+++ server.py
@@ -1,8 +1,24 @@
 import os
 import time
 import socket
+import platform
 
-os.system('clear')
+OSname = str(platform.system())
+
+if OSname == "Windows":
+
+	def clear():
+
+		import os
+		os.system('cls')
+else:
+
+	def clear():
+
+		import os
+		os.system('clear')
+
+clear()
 
 s = socket.socket()
 host = socket.gethostname()
@@ -12,12 +28,12 @@ s.listen(1)
 xhost = str(host)
 print("["+xhost+"]: Waiting for incoming connections..")
 conn, addr = s.accept()
-os.system('clear')
+clear()
 print(addr, "Has connected! Press [ANY-KEY] to send data..")
 input(" ")
 filename = str("data.rex")
 file = open(filename, 'rb')
 file_data = file.read(1024)
 conn.send(file_data)
-os.system('clear')
+clear()
 print("Data has been transmitted!")
